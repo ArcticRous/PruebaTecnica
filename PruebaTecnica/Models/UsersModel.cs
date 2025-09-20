a4386e868244d641de2383fe5ade92e207d37c85
ArcticRous <alandelarosa17@gmail.com>
2025-09-20 15:00:11 -0600

    Creacion de modelo de usuario con atributos de validación

---
+++ PruebaTecnica/Models/UsersModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PruebaTecnica.Models
+{
+    public class UsersModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
